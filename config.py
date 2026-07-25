23d828857e1c39f86068b53d5bb1d8760d39b3b0
aurimardelosangelesgarayarnias-droid <aurimardelosangelesgarayarnias@gmail.com>
2026-07-25 13:48:57 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+# Token del bot de Telegram
+TOKEN = os.getenv("BOT_TOKEN")
+
+
+# Datos Voxura
+
+ANGIE_USERNAME = "@Adga_09"
+
+CANAL_CAPACITACION = "https://t.me/+zUiMto2pLb01N2Fh"
+
+CANAL_CONVIVENCIA = "https://t.me/+hQt1ioFPYoFmZjBh"
