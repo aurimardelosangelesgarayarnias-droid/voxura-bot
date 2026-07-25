57b085db0bfdfa74e27f54d8ed1c80a06da0a701
aurimardelosangelesgarayarnias-droid <aurimardelosangelesgarayarnias@gmail.com>
2026-07-25 13:50:47 -0400

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,51 @@
+BIENVENIDA = """
+
+Hola hermosa 💙 soy Aura,
+la asistente virtual de Voxura ✨
+
+
+Me alegra mucho que estés aquí.
+
+Estoy para ayudarte a conocer nuestra agencia
+y orientarte sobre esta oportunidad dentro de BuzzCast.
+
+
+Antes de contarte más, quiero conocerte un poquito 💙
+
+"""
+
+
+PEDIR_NOMBRE = """
+
+Qué lindo conocerte 💙
+
+¿Cuál es tu nombre?
+
+"""
+
+
+PEDIR_EDAD = """
+
+Perfecto ✨
+
+¿Cuántos años tienes?
+
+"""
+
+
+PEDIR_UBICACION = """
+
+Gracias por contarme 💙
+
+¿De qué ciudad o país eres?
+
+"""
+
+
+PEDIR_MOTIVACION = """
+
+Y cuéntame algo importante ✨
+
+¿Qué te gustaría conseguir formando parte de Voxura?
+
+"""
