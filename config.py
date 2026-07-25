b770d302073ed87d5d56105ac8410f91baea07d4
aurimardelosangelesgarayarnias-droid <aurimardelosangelesgarayarnias@gmail.com>
2026-07-25 14:26:48 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,8 @@ import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-
-# Datos de líder Voxura
-
-ANGIE_USERNAME = "@Adga_09"
-
 ANGIE_ID = 1621617481
-
-
-# Canales oficiales Voxura
+ANGIE_USERNAME = "@Adga_09"
 
 CANAL_CAPACITACION = "https://t.me/+zUiMto2pLb01N2Fh"
 
